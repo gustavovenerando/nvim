c8d5f7f41f0573e760cdd945adb5391301248aa2
gustavovenerando <gvhenrique@hotmail.com>
2023-09-17 20:45:18 -0300

    v.1.2.2: change indent color

--- after/plugin/indent.lua
+++ after/plugin/indent.lua
@@ -1,4 +1,11 @@
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#aba9a9 gui=nocombine]]
+
+
 require("indent_blankline").setup {
     show_trailing_blankline_indent = false,
     show_current_context = false,
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+    }
 }
