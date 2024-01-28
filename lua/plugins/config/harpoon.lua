65e8fe42025582608fa2e7656309b84c16555e7b
gustavovenerando <gvhenrique@hotmail.com>
2024-01-28 14:27:11 -0300

    v4.7: added harpoon remaps for terminal

--- lua/plugins/config/harpoon.lua
+++ lua/plugins/config/harpoon.lua
@@ -7,15 +7,14 @@ require("harpoon").setup({
 
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local term = require("harpoon.term")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 
-vim.keymap.set("n", "<C-i>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<A-u>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<A-i>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<A-o>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<A-p>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>j1", function() term.gotoTerminal(1) end)
+vim.keymap.set("n", "<leader>j2", function() term.gotoTerminal(2) end)
+vim.keymap.set("n", "<leader>j3", function() term.gotoTerminal(3) end)
+vim.keymap.set("n", "<leader>j4", function() term.gotoTerminal(4) end)
 
 vim.keymap.set("n", "<A-l>", ui.nav_next)
 vim.keymap.set("n", "<A-h>", ui.nav_prev)
