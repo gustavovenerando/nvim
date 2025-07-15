02917384a5af3ed1a4534108141242a43e34f430
gustavovenerando <gvhenrique@hotmail.com>
2025-07-14 21:27:40 -0300

    v8.5: identation fix and changed 42 header remap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,69 +15,69 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 return require("lazy").setup({
-	{
-		"nvim-telescope/telescope.nvim",
-		event = "VimEnter",
+    {
+        "nvim-telescope/telescope.nvim",
+        event = "VimEnter",
         branch = "master",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"debugloop/telescope-undo.nvim",
-		},
-		config = function()
-			require("plugins.config.telescope")
-		end,
-	},
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "debugloop/telescope-undo.nvim",
+        },
+        config = function()
+            require("plugins.config.telescope")
+        end,
+    },
 
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("plugins.config.colors")
-		end,
-	},
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("plugins.config.colors")
+        end,
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"nvim-treesitter/nvim-treesitter-context",
-		},
-		config = function()
-			require("plugins.config.treesitter")
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = "VeryLazy",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/nvim-treesitter-context",
+        },
+        config = function()
+            require("plugins.config.treesitter")
+        end,
+    },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		config = function()
-			require("plugins.config.indent")
-		end,
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("plugins.config.indent")
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
             "williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-			{ "j-hui/fidget.nvim", opts = {} },
+            { "j-hui/fidget.nvim", opts = {} },
 
-			-- Schema information
-			"b0o/SchemaStore.nvim",
-		},
-		config = function()
-			require("plugins.config.lsp")
-		end,
-	},
+            -- Schema information
+            "b0o/SchemaStore.nvim",
+        },
+        config = function()
+            require("plugins.config.lsp")
+        end,
+    },
 
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
             {
                 "L3MON4D3/LuaSnip",
                 build = "make install_jsregexp",
@@ -90,115 +90,115 @@ return require("lazy").setup({
                     },
                 }
             },
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-omni",
-			"hrsh7th/cmp-emoji",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-omni",
+            "hrsh7th/cmp-emoji",
             "saadparwaiz1/cmp_luasnip",
             "onsails/lspkind.nvim"
-		},
-		config = function()
-			require("plugins.config.cmp")
-		end,
-	},
+        },
+        config = function()
+            require("plugins.config.cmp")
+        end,
+    },
 
     {
         "stevearc/oil.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-			require("plugins.config.oil")
+            require("plugins.config.oil")
         end,
     },
 
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("plugins.config.tree")
-		end,
-	},
-
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("plugins.config.comment")
-		end,
-	},
-
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("plugins.config.gitsigns")
-		end,
-	},
-
-	{
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("plugins.config.toggleterm")
-		end,
-	},
-
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-		config = function()
-			require("plugins.config.lualine")
-		end,
-	},
-
-	{
-		"ThePrimeagen/harpoon",
-		dependencies = { "nvim-lua/plenary.nvim" },
-        commit = "ccae1b9bec717ae284906b0bf83d720e59d12b91",
-		config = function()
-			require("plugins.config.harpoon")
-		end,
-	},
-
-	{
-		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		config = function()
-			require("lsp_lines").setup()
-		end,
-	},
-
-	{
-		"goolord/alpha-nvim",
-		config = function()
-			require("plugins.config.alpha")
-		end,
-	},
-
-	{
-		"gbprod/yanky.nvim",
-		config = function()
-			require("plugins.config.yanky")
-		end,
-	},
-
-	{
-		"tpope/vim-fugitive",
-		config = function()
-			require("plugins.config.fugitive")
-		end,
-	},
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("plugins.config.tree")
+        end,
+    },
 
     {
-	    "sindrets/diffview.nvim",
+        "numToStr/Comment.nvim",
+        config = function()
+            require("plugins.config.comment")
+        end,
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("plugins.config.gitsigns")
+        end,
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("plugins.config.toggleterm")
+        end,
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+        config = function()
+            require("plugins.config.lualine")
+        end,
+    },
+
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        commit = "ccae1b9bec717ae284906b0bf83d720e59d12b91",
+        config = function()
+            require("plugins.config.harpoon")
+        end,
+    },
+
+    {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+        end,
+    },
+
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require("plugins.config.alpha")
+        end,
+    },
+
+    {
+        "gbprod/yanky.nvim",
+        config = function()
+            require("plugins.config.yanky")
+        end,
+    },
+
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            require("plugins.config.fugitive")
+        end,
+    },
+
+    {
+        "sindrets/diffview.nvim",
         config = function()
             require("diffview").setup({
                 file_panel = {
@@ -211,27 +211,27 @@ return require("lazy").setup({
         end
     },
 
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {},
-	},
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {},
+    },
 
-	{
-		"smjonas/inc-rename.nvim",
-		config = function()
-			require("inc_rename").setup()
-		end,
-	},
+    {
+        "smjonas/inc-rename.nvim",
+        config = function()
+            require("inc_rename").setup()
+        end,
+    },
 
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = {
-			"kevinhwang91/promise-async",
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
             {
                 "luukvbaal/statuscol.nvim",
                 config = function()
@@ -240,16 +240,16 @@ return require("lazy").setup({
                         {
                             relculright = true,
                             segments = {
-                                { text = { builtin.foldfunc },    click = "v:lua.ScFa" },
+                                { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
                                 { text = { " " } },
-                                { text = { "%s" },                click = "v:lua.ScSa" },
+                                { text = { "%s" },                  click = "v:lua.ScSa" },
                                 { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
                             }
                         }
                     )
                 end,
             },
-		},
+        },
         config = function()
             local handler = function(virtText, lnum, endLnum, width, truncate)
                 local newVirtText = {}
@@ -265,7 +265,7 @@ return require("lazy").setup({
                     else
                         chunkText = truncate(chunkText, targetWidth - curWidth)
                         local hlGroup = chunk[2]
-                        table.insert(newVirtText, {chunkText, hlGroup})
+                        table.insert(newVirtText, { chunkText, hlGroup })
                         chunkWidth = vim.fn.strdisplaywidth(chunkText)
                         -- str width returned from truncate() may less than 2nd argument, need padding
                         if curWidth + chunkWidth < targetWidth then
@@ -275,7 +275,7 @@ return require("lazy").setup({
                     end
                     curWidth = curWidth + chunkWidth
                 end
-                table.insert(newVirtText, {suffix, 'MoreMsg'})
+                table.insert(newVirtText, { suffix, 'MoreMsg' })
                 return newVirtText
             end
 
@@ -286,7 +286,7 @@ return require("lazy").setup({
                 fold_virt_text_handler = handler
             })
         end,
-	},
+    },
 
     {
         "pmizio/typescript-tools.nvim",
@@ -294,16 +294,16 @@ return require("lazy").setup({
         opts = {},
     },
 
-	"eandrju/cellular-automaton.nvim",
-	"windwp/nvim-ts-autotag",
-	"ton/vim-bufsurf",
+    "eandrju/cellular-automaton.nvim",
+    "windwp/nvim-ts-autotag",
+    "ton/vim-bufsurf",
 
     {
         "Diogo-ss/42-header.nvim",
         cmd = { "Stdheader" },
-        keys = { "<F1>" },
+        keys = { "<F3>" },
         opts = {
-            default_map = true, -- Default mapping <F1> in normal mode.
+            default_map = true,  -- Default mapping <F1> in normal mode.
             auto_update = false, -- Update header when saving.
             -- user = "username", -- Your user.
             -- mail = "your@email.com", -- Your mail.
