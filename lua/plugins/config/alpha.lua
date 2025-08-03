733600a6f1aef4c5abe96a59fea02598ef73f84c
gustavovenerando <gvhenrique@hotmail.com>
2025-08-03 20:10:45 -0300

    new quotes

--- lua/plugins/config/alpha.lua
+++ lua/plugins/config/alpha.lua
@@ -663,6 +663,9 @@ local fortune_list = {
     {"Be as you wish to seem.", "", "- Socrates"},
     {'Do, or do not. There is no “try"', "", "- Yoda"},
     {"Life moves pretty fast. If you don’t stop and look around once in a while, you could miss it.", "", "- Ferris Bueller"},
+    {"Defeat is a state of mind; no one is ever defeated until defeat has been accepted as a reality.", "", "- Bruce Lee"},
+    {"I fear not the man who has practiced 10,000 kicks once, but I fear the man who had practiced one kick 10,000 times.", "", "- Bruce Lee"},
+    {"If you spend too much time thinking about a thing, you'll never get it done.", "", "- Bruce Lee"},
 }
 
 local fortune = function(opts)
