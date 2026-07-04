e61f442c015e72b3d1c608381a1fed9922ffdc3a
gustavovenerando <gvhenrique@hotmail.com>
2026-07-04 16:58:47 -0300

    v9.6: harpoon dynamic menu size and harpoon menu cursor initialization

--- lua/plugins/config/harpoon.lua
+++ lua/plugins/config/harpoon.lua
@@ -1,15 +1,27 @@
 require("harpoon").setup({
     menu = {
-        width = vim.api.nvim_win_get_width(0) - 70,
-        height = vim.api.nvim_win_get_height(0) - 40,
-    }
+        width = math.floor(vim.o.columns * 0.45),
+        height = math.floor(vim.o.lines * 0.25),
+    },
 })
 
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[Harpoon] Add file" })
-vim.keymap.set("n", "<C-f>", ui.toggle_quick_menu, { desc = "[Harpoon] Toggle quick menu" })
+
+vim.keymap.set("n", "<C-f>", function()
+    ui.toggle_quick_menu()
+
+    vim.schedule(function()
+        local buf = vim.api.nvim_get_current_buf()
+
+        if vim.bo[buf].filetype == "harpoon" then
+            local row = vim.api.nvim_win_get_cursor(0)[1]
+            vim.api.nvim_win_set_cursor(0, { row, 0 })
+        end
+    end)
+end, { desc = "[Harpoon] Toggle quick menu" })
 
 vim.keymap.set("n", "<A-l>", ui.nav_next, { desc = "[Harpoon] Next" })
 vim.keymap.set("n", "<A-h>", ui.nav_prev, { desc = "[Harpoon] Previous" })
