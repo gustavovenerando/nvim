99c6bce7e81174e54e4dd1c5bb856fb6133aa295
gustavovenerando <gvhenrique@hotmail.com>
2026-09-13 20:14:24 -0300

    v10.1: rewriting tree-sitter config and adding incrementalselection

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -20,25 +20,30 @@ local parsers = {
 
 require("nvim-treesitter").install(parsers)
 
+-- Treesitter highlighting + indentation
 local group = vim.api.nvim_create_augroup("user_treesitter", {
     clear = true,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     group = group,
+
     callback = function(args)
         local ok = pcall(vim.treesitter.start, args.buf)
 
         if ok and vim.bo[args.buf].filetype ~= "python" then
-            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            vim.bo[args.buf].indentexpr =
+                "v:lua.require'nvim-treesitter'.indentexpr()"
         end
     end,
 })
 
+-- Treesitter textobjects
 require("nvim-treesitter-textobjects").setup({
     select = {
         lookahead = true,
     },
+
     move = {
         set_jumps = true,
     },
@@ -48,62 +53,46 @@ local select = require("nvim-treesitter-textobjects.select")
 local move = require("nvim-treesitter-textobjects.move")
 local swap = require("nvim-treesitter-textobjects.swap")
 
-vim.keymap.set({ "x", "o" }, "aa", function()
-    select.select_textobject("@parameter.outer", "textobjects")
-end)
+-- Textobject selection
+local textobjects = {
+    aa = "@parameter.outer",
+    ia = "@parameter.inner",
+    af = "@function.outer",
+    ["if"] = "@function.inner",
+    ac = "@class.outer",
+    ic = "@class.inner",
+}
 
-vim.keymap.set({ "x", "o" }, "ia", function()
-    select.select_textobject("@parameter.inner", "textobjects")
-end)
+for key, query in pairs(textobjects) do
+    vim.keymap.set({ "x", "o" }, key, function()
+        select.select_textobject(query, "textobjects")
+    end)
+end
 
-vim.keymap.set({ "x", "o" }, "af", function()
-    select.select_textobject("@function.outer", "textobjects")
-end)
+-- Textobject movement
+local movements = {
+    ["]m"] = { move.goto_next_start, "@function.outer" },
+    ["]]"] = { move.goto_next_start, "@class.outer" },
 
-vim.keymap.set({ "x", "o" }, "if", function()
-    select.select_textobject("@function.inner", "textobjects")
-end)
+    ["]M"] = { move.goto_next_end, "@function.outer" },
+    ["]["] = { move.goto_next_end, "@class.outer" },
 
-vim.keymap.set({ "x", "o" }, "ac", function()
-    select.select_textobject("@class.outer", "textobjects")
-end)
+    ["[m"] = { move.goto_previous_start, "@function.outer" },
+    ["[["] = { move.goto_previous_start, "@class.outer" },
 
-vim.keymap.set({ "x", "o" }, "ic", function()
-    select.select_textobject("@class.inner", "textobjects")
-end)
+    ["[M"] = { move.goto_previous_end, "@function.outer" },
+    ["[]"] = { move.goto_previous_end, "@class.outer" },
+}
 
-vim.keymap.set({ "n", "x", "o" }, "]m", function()
-    move.goto_next_start("@function.outer", "textobjects")
-end)
+for key, mapping in pairs(movements) do
+    local fn, query = unpack(mapping)
 
-vim.keymap.set({ "n", "x", "o" }, "]]", function()
-    move.goto_next_start("@class.outer", "textobjects")
-end)
+    vim.keymap.set({ "n", "x", "o" }, key, function()
+        fn(query, "textobjects")
+    end)
+end
 
-vim.keymap.set({ "n", "x", "o" }, "]M", function()
-    move.goto_next_end("@function.outer", "textobjects")
-end)
-
-vim.keymap.set({ "n", "x", "o" }, "][", function()
-    move.goto_next_end("@class.outer", "textobjects")
-end)
-
-vim.keymap.set({ "n", "x", "o" }, "[m", function()
-    move.goto_previous_start("@function.outer", "textobjects")
-end)
-
-vim.keymap.set({ "n", "x", "o" }, "[[", function()
-    move.goto_previous_start("@class.outer", "textobjects")
-end)
-
-vim.keymap.set({ "n", "x", "o" }, "[M", function()
-    move.goto_previous_end("@function.outer", "textobjects")
-end)
-
-vim.keymap.set({ "n", "x", "o" }, "[]", function()
-    move.goto_previous_end("@class.outer", "textobjects")
-end)
-
+-- Swap parameters
 vim.keymap.set("n", "<leader>x", function()
     swap.swap_next("@parameter.inner")
 end)
@@ -112,6 +101,20 @@ vim.keymap.set("n", "<leader>X", function()
     swap.swap_previous("@parameter.inner")
 end)
 
+-- Incremental selection
+vim.keymap.set({ "n", "x" }, "<C-Space>", function()
+    vim.treesitter.select("parent", vim.v.count1)
+end, {
+    desc = "Expand Tree-sitter selection",
+})
+
+vim.keymap.set("x", "<C-h>", function()
+    vim.treesitter.select("child", vim.v.count1)
+end, {
+    desc = "Shrink Tree-sitter selection",
+})
+
+-- Treesitter context
 require("treesitter-context").setup({
     enable = true,
     multiwindow = false,
